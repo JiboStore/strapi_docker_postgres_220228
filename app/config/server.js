287d1a50bc63fb986a5b67cec7f2fbaaa0941521
Hakim Hauston <hakim.hauston@storms.com>
2022-02-28 18:18:19 +0800

    running with docker-compose
    -. docker-compose up

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2c2bb255ff4abbece62d30551e4cc980'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1280a148-f107-44a8-b78e-617d09afcddf'
+};
